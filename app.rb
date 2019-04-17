63e88ab1a9395cca434a61218f17dc42061c96e2
blackwell <blackwellsmithiv@gmail.com>
2019-04-17 23:09:21 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+  
+  get '/' do 
+    erb :index
+  end
+  
+  get '/new' do
+    erb :create_puppy
+  end
+  
+  post '/puppy' do
+    
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    @puppy = Puppy.new(@name, @breed, @age)
+#binding.pry
+    erb :display_puppy
+  end
 
 end
